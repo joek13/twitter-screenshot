72460ebb0241d51c60f9b4485027b2b0abedfbf2
Joe Kerrigan <joek1301@gmail.com>
2023-05-15 10:18:10 -0400

    Add script to hide UI elements from Tweet

---
+++ app/screenshotter/hide_elements.js
@@ -0,0 +1,19 @@
+// hides elements from tweet UI to make prettier screenshots
+
+// thanks https://stackoverflow.com/questions/10596417/is-there-a-way-to-get-element-by-xpath-using-javascript-in-selenium-webdriver
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+const TARGETS = [
+    // bullet before number of views
+    "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[5]/div/div[1]/div/div[2]",
+    // number of views
+    "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[5]/div/div[1]/div/div[3]",
+    // retweets/quotes/likes/bookmarks stats bar
+    "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[6]",
+    // reply/retweet/like/bookmmark/share actions bar
+    "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[7]"
+];
+
+TARGETS.forEach(elem_xpath => getElementByXpath(elem_xpath).style.visibility = "hidden");
--- app/screenshotter/twitter.py
+++ app/screenshotter/twitter.py
@@ -6,9 +6,16 @@ import time
 from selenium.webdriver.common.by import By
 from PIL import Image
 from io import BytesIO
+from pathlib import Path
 
 _ZOOM_FACTOR = 1
 TWEET_URL_REGEX = re.compile(r"(?:https?://)?twitter\.com/[a-zA-Z_]+/status/\d+", re.MULTILINE)
+
+_HIDE_ELEMENTS_SCRIPT = ""
+with open(Path(__file__).resolve().parent.joinpath("hide_elements.js"), "r") as f:
+    _HIDE_ELEMENTS_SCRIPT = f.read() # load js to hide ui elements
+
+_IMAGE_CROP_OFFSET = (0, 0, 0, -110) # remove whitespace where ui elements were
 
 browser = driver.get_driver()
 
@@ -21,6 +28,7 @@ def screenshot_tweet(tweet_url: str) -> Image.Image:
     browser.get(tweet_url)
     time.sleep(3)
     browser.execute_script(f"document.body.style.zoom='{_ZOOM_FACTOR * 100}%'") # hack to make tweet larger
+    browser.execute_script(_HIDE_ELEMENTS_SCRIPT) # hide some ui elements from screenshot
 
     tweet = browser.find_element(By.TAG_NAME, "article")
     loc = tweet.location
@@ -32,10 +40,10 @@ def screenshot_tweet(tweet_url: str) -> Image.Image:
     # for whatever reason, the window is exactly twice as large as requested,
     # even when --device-scale-factor=1 is set.
     bounds = (
-        _ZOOM_FACTOR * 2 * loc["x"],
-        _ZOOM_FACTOR * 2 * loc["y"],
-        _ZOOM_FACTOR * 2 * (loc["x"] + size["width"]),
-        _ZOOM_FACTOR * 2 * (loc["y"] + size["height"])
+        _ZOOM_FACTOR * 2 * (loc["x"] + _IMAGE_CROP_OFFSET[0]),
+        _ZOOM_FACTOR * 2 * (loc["y"] + _IMAGE_CROP_OFFSET[1]),
+        _ZOOM_FACTOR * 2 * (loc["x"] + size["width"] + _IMAGE_CROP_OFFSET[2]),
+        _ZOOM_FACTOR * 2 * (loc["y"] + size["height"] + _IMAGE_CROP_OFFSET[3])
     )
     cropped = img.crop(bounds)
 
