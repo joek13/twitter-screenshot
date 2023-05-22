e9415f31d48f0b08a166a4547871a33c203dff1c
Joe Kerrigan <joek1301@gmail.com>
2023-05-22 18:26:11 -0400

    Remove accidentally committed test file

--- app/screenshotter/test.py
+++
@@ -1,7 +0,0 @@
-from . import driver
-
-if __name__ == "__main__":
-    driver = driver.get_driver()
-    driver.get("https://news.ycombinator.com")
-    driver.save_screenshot("/tmp/screenshot.png")
-    driver.close()
