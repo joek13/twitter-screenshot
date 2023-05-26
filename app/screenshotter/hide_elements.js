6361d663d26310b27a7cb3a1b6e0057b00abb53e
Joe Kerrigan <joek1301@gmail.com>
2023-05-26 14:07:43 -0400

    Hide signup banner

--- app/screenshotter/hide_elements.js
+++ app/screenshotter/hide_elements.js
@@ -13,7 +13,9 @@ const TARGETS = [
     // retweets/quotes/likes/bookmarks stats bar
     "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[6]",
     // reply/retweet/like/bookmmark/share actions bar
-    "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[7]"
+    "/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[7]",
+    // signup banner
+    "/html/body/div[1]/div/div/div[1]/div/div[1]/div/div"
 ];
 
 TARGETS.forEach(elem_xpath => {
